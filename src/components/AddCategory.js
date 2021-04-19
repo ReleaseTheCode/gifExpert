5630da6896286db0318a8be9374631ac3c158d88
Richard n.n <richard.nunez@readinessit.com>
2021-04-19 01:34:58 -0400

    Render images fetched from gif api

---
+++ src/components/AddCategory.js
@@ -0,0 +1,27 @@
+import React, { useState } from 'react'
+import PropTypes from 'prop-types';
+export const AddCategory = ( { setCategories } ) => {
+    const [inputValue, setInputValue] = useState('');
+    const handleInputChange = (event) => {
+        setInputValue(event.target.value.trim());
+    }
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        if(inputValue.trim().length > 2 ){
+            setCategories( categories => [...categories,inputValue] );
+            setInputValue('');
+        }
+    }
+    return (
+        <form onSubmit={handleSubmit}>
+            <input
+                type="text"
+                value={ inputValue }
+                onChange={ handleInputChange }
+            />
+        </form>
+    )
+}
+AddCategory.propTypes = {
+    setCategories: PropTypes.func.isRequired
+}
